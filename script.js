3edaa623daa794be3a5da7e53b0be4572ea28343
Kunsguru <kingsleym2024@gmail.com>
2024-06-15 19:48:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 document.getElementById('summitButton').addEventListener('click', function() {
     var email = 'artsbasseyartsbassey@gmail.com';
-    var subject = 'Help desk';
-    var body = 'Hello,\n\n.
-        
-
-            I'm Mike,\n,\n.
-    How can I be of help to you?,\n\n.
-        
-        
-    
-Thank you.';
+    var subject = 'Help Desk';
+    var body = 'Hello,\n\I would like to make an inquiry more?.\n\nThank you.';
     
     var mailtoLink = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
     
     window.location.href = mailtoLink;
 });
+
+
+
 
 
