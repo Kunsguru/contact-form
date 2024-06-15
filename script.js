3916b8a3a6d5ba5541c31709d97d439ef1471078
Kunsguru <kingsleym2024@gmail.com>
2024-06-15 22:40:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 document.getElementById('summitButton').addEventListener('click', function() {
-    var email = 'artsbasseyartsbassey@gmail.com';
-    var subject = 'Help Desk';
-    var body = 'Hello,\n\nYou want  to make an inquiry?.\n\nI am here to help you.';
-    var mailtoLink = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
-    
-    window.location.href = mailtoLink;
-});
+            document.getElementById('emailForm').classList.remove('hidden');
+        });
 
+        document.getElementById('sendEmailButton').addEventListener('click', function() {
+            var email = 'artsbasseyartsbassey@gmail.com';
+            var subject = 'Help Desk';
+            var userMessage = document.getElementById('userMessage').value;
+            var mailtoLink = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(userMessage);
+            
+            window.location.href = mailtoLink;
+        });
 
-document.getElementById("homeButton").onclick = function() {
+        document.getElementById("homeButton").onclick = function() {
             window.location.href = "https://aaartswithartsbasy2024.onrender.com/";
         }
 
