bec69ad25f6b1f42e77dc88a7d84f2b22818b892
Kunsguru <kingsleym2024@gmail.com>
2024-06-15 20:08:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.getElementById('summitButton').addEventListener('click', function() {
     var email = 'artsbasseyartsbassey@gmail.com';
     var subject = 'Help Desk';
-    var body = 'Hello,\n\I would like to make an inquiry more?.\n\nThank you.';
+    var body = 'Hello,\n\I'm Mike.\n\
+  I would like to help you answer your questions?.\n\nThank you.';
     
     var mailtoLink = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(body);
     
